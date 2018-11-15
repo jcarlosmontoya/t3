5e73973d12f66587604570dc7a59a53b5faef8b4
jcarlosmontoya <jcarlosmontoyach@gmail.com>
2018-11-15 01:04:26 +0100

    Imp mod gest viajes

--- gestion_viajes/models/conductor.py
+++ gestion_viajes/models/conductor.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api, fields, exceptions, models
 
 
 class Conductor(models.Model):
@@ -21,6 +21,32 @@ class Conductor(models.Model):
         compute="_get_truck_count",
         string="Cuantos vehiculos",
     )
+
+    licencia = fields.Char("Licencia conducir", size=8)
+
+    fecha_caducidad = fields.Date("Fecha caducidad licencia")
+
+    @api.model
+    def default_get(self, values):
+        res = super(Conductor, self).default_get(values)
+        # por defecto un nuevo conductor es de El Salvador
+        # search, search_read, browse
+        domain = [['code', '=', 'SV']]
+        country_search = self.env['res.country'].search(domain, limit=1)
+        country_browse = self.env['res.country'].browse([209])
+        country_search_read = self.env['res.country'].search_read(
+            domain, ['name', 'code'])
+        if country_search:
+            res['pais'] = country_search.id
+
+        return res
+
+    @api.onchange("pais")
+    def onchange_pais(self):
+        if self.fecha_caducidad:
+            if self.fecha_caducidad < fields.Date.today():
+                raise exceptions.Warning("Ojo la licencia esta caducada")
+        stop = True
 
     def _get_truck_count(self):
         for conductor in self:
